46d1516f58e34aa3b727f169928d4e907856aa73
crimtin <christianmck@hotmail.com>
2022-06-02 01:19:25 -0500

    cleaned up code by merging two functions. dynamically displying the list of tv search results.

--- src/TVInfo.tsx
+++ src/TVInfo.tsx
@@ -7,27 +7,37 @@ const TVInfo = () => {
   const [searchTitle, setSearchTitle] = useState("");
   const [searchResults, setSearchResults] = useState<ApiSearchResponse>();
 
-  const searchTitleInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+  const searchTitleInputHandler = async (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
     setSearchTitle(event.target.value);
-    console.log(searchTitle);
-  };
-
-  const getTVSearchInfo = async () => {
     const tvSearchInfo: ApiSearchResponse = await ky
       .get(
         `https://api.themoviedb.org/3/search/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1&query=${searchTitle}&include_adult=false`
       )
       .json();
-    console.log(tvSearchInfo.results[0].id);
 
-    // setSearchResults(tvSearchInfo);
+    setSearchResults(tvSearchInfo);
   };
+
+  const tvSearchList = () => {
+    return (
+      <div className="listWrapper">
+        <ul>
+          {searchResults?.results.map((searchResult, index) => (
+            <li key={searchResult.id}>{searchResult.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
   console.log(searchResults);
-  getTVSearchInfo();
 
   return (
     <div className="moviePageWrapper">
       <input type="text" onChange={searchTitleInputHandler} />
+      {tvSearchList()}
     </div>
   );
 };
