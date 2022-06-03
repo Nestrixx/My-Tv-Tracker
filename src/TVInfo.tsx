88fbe5b754f6d7f4782535ac48cda115f2bf52e5
crimtin <christianmck@hotmail.com>
2022-06-02 23:24:14 -0500

    cleaned up code and added useEffect to the api call.

--- src/TVInfo.tsx
+++ src/TVInfo.tsx
@@ -1,5 +1,5 @@
 import ky from "ky";
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, useEffect } from "react";
 import "./TVInfo.scss";
 import { ApiSearchResponse } from "./types/SearchTvResponse";
 
@@ -7,37 +7,28 @@ const TVInfo = () => {
   const [searchTitle, setSearchTitle] = useState("");
   const [searchResults, setSearchResults] = useState<ApiSearchResponse>();
 
-  const searchTitleInputHandler = async (
-    event: ChangeEvent<HTMLInputElement>
-  ) => {
-    setSearchTitle(event.target.value);
-    const tvSearchInfo: ApiSearchResponse = await ky
-      .get(
-        `https://api.themoviedb.org/3/search/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1&query=${searchTitle}&include_adult=false`
-      )
-      .json();
+  useEffect(() => {
+    const searchTitleInputHandler = async () => {
+      const tvSearchInfo: ApiSearchResponse = await ky
+        .get(
+          `https://api.themoviedb.org/3/search/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1&query=${searchTitle}&include_adult=false`
+        )
+        .json();
+      setSearchResults(tvSearchInfo);
+    };
+    searchTitleInputHandler();
+  }, [searchTitle]);
 
-    setSearchResults(tvSearchInfo);
-  };
-
-  const tvSearchList = () => {
-    return (
+  return (
+    <div className="moviePageWrapper">
+      <input type="text" onChange={(e) => setSearchTitle(e.target.value)} />
       <div className="listWrapper">
         <ul>
-          {searchResults?.results.map((searchResult, index) => (
+          {searchResults?.results.map((searchResult) => (
             <li key={searchResult.id}>{searchResult.name}</li>
           ))}
         </ul>
       </div>
-    );
-  };
-
-  console.log(searchResults);
-
-  return (
-    <div className="moviePageWrapper">
-      <input type="text" onChange={searchTitleInputHandler} />
-      {tvSearchList()}
     </div>
   );
 };
